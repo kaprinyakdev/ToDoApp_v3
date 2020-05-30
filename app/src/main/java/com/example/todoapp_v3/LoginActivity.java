df3315af0a29e799daac20db190876f689c52b4d
peter <kaprinyakdev@gmail.com>
2020-05-30 20:25:02 +0200

    google sing in test

---
+++ app/src/main/java/com/example/todoapp_v3/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.todoapp_v3;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+
+
+    }
+}
